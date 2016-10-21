0291366b3da19c7ee89c9cafd27043952fbe688c
jane <jane@email.com>
2016-10-21 15:22:38 +0300

    Тест: "Проверка добавления работника"

---
+++ department/UnitTestProject1/UnitTestProject1/Employed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UnitTestProject1
+{
+    internal class Employed : Person
+    {
+        public int selary { get; private set; }
+
+        public object position { get; private set; }
+
+        internal void setPerson(Person vPupkin)
+        {
+            this.firstName = vPupkin.firstName;
+            this.secondName = vPupkin.secondName;
+            this.gender = vPupkin.gender;
+            this.age = vPupkin.age;
+            
+        }
+
+        internal void setSalary(int v)
+        {
+            this.selary = v;
+        }
+
+        internal void setPosition(string v)
+        {
+            this.position = v;
+        }
+    }
+}
