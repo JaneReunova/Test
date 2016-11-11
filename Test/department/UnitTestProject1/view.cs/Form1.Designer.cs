077501aaf14a8c7f245a1ad56866ebc5045b93e4
jane <jane@email.com>
2016-11-11 14:44:27 +0300

    добавила ComboBox, GroupBox, TextBox, TextBox

--- Test/department/UnitTestProject1/view.cs/Form1.Designer.cs
+++ Test/department/UnitTestProject1/view.cs/Form1.Designer.cs
@@ -29,11 +29,17 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBoxEmployed = new System.Windows.Forms.GroupBox();
+            this.textBoxPosition = new System.Windows.Forms.TextBox();
+            this.comboBoxPerson = new System.Windows.Forms.ComboBox();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBoxEmployed.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -49,27 +55,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBoxPerson";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(0, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(0, 55);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(0, 91);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 2;
-            // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
@@ -81,16 +66,84 @@
             this.comboBox1.Size = new System.Drawing.Size(98, 21);
             this.comboBox1.TabIndex = 3;
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(0, 91);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBoxEmployed
+            // 
+            this.groupBoxEmployed.Controls.Add(this.buttonAdd);
+            this.groupBoxEmployed.Controls.Add(this.textBoxSalary);
+            this.groupBoxEmployed.Controls.Add(this.comboBoxPerson);
+            this.groupBoxEmployed.Controls.Add(this.textBoxPosition);
+            this.groupBoxEmployed.Location = new System.Drawing.Point(13, 189);
+            this.groupBoxEmployed.Name = "groupBoxEmployed";
+            this.groupBoxEmployed.Size = new System.Drawing.Size(150, 161);
+            this.groupBoxEmployed.TabIndex = 1;
+            this.groupBoxEmployed.TabStop = false;
+            this.groupBoxEmployed.Text = "groupBoxEmployed";
+            // 
+            // textBoxPosition
+            // 
+            this.textBoxPosition.Location = new System.Drawing.Point(6, 55);
+            this.textBoxPosition.Name = "textBoxPosition";
+            this.textBoxPosition.Size = new System.Drawing.Size(121, 20);
+            this.textBoxPosition.TabIndex = 0;
+            // 
+            // comboBoxPerson
+            // 
+            this.comboBoxPerson.FormattingEnabled = true;
+            this.comboBoxPerson.Location = new System.Drawing.Point(6, 28);
+            this.comboBoxPerson.Name = "comboBoxPerson";
+            this.comboBoxPerson.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPerson.TabIndex = 1;
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.Location = new System.Drawing.Point(6, 81);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.Size = new System.Drawing.Size(121, 20);
+            this.textBoxSalary.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(34, 117);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 362);
+            this.Controls.Add(this.groupBoxEmployed);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "FormG";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBoxEmployed.ResumeLayout(false);
+            this.groupBoxEmployed.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -102,6 +155,11 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBoxEmployed;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.ComboBox comboBoxPerson;
+        private System.Windows.Forms.TextBox textBoxPosition;
     }
 }
 
